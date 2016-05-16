d31f91e755b60735d861f6c91a89df0243b82acf
Marc <phyrenight@gmail.com>
2016-05-16 04:49:29 -0400

    feat added tournamentStartMenu()
    This menu is used to display current matchups, player standings
     and to update matches.

--- index.py
+++ index.py
@@ -23,13 +23,7 @@ def menu():
                 print "Insufficient number of players. Tournament can not be started."
             else:
             	choice = 'q'
-                #tournamentPlay()
-                #the processes in this else are just for testing
-                pairs = []
-                pairs = tournament.swissPairings()
-                print "Current matches are:"
-                for i in pairs:
-                    print i
+                startTournamentMenu()
         elif choice == '3':
         	players = tournament.playerStandings()
         	for i in players:  # try turning these two lines into a list comprehension
@@ -51,32 +45,68 @@ def menu():
 
 
 def displayStandings():
+    """
+        tournament standings are print to the screen
+    """
     results =  tournament.playerStandings()
     print "Current standings are: "
     for i in results:
-        print i
+        print "id:{} name: {} wins: {} loses: {} ".format(i[0], i[1], i[2], i[3])
 
 def updateMatches():
-    for i in tournament.swissPairings():
-        print i
+    """
+        Used to to get wins/loses of the players. 
+    """
+    displayCurrentMatches()
     print "Please enter the winners id "
     winner = int(raw_input("> ")) # add test make sure id is in tournament
     print "Print enter the losers id "
-    loser = int(raw_input("> ")) # add test to make sure id is assigned to a player
+    loser = int(raw_input("> ")) #1 add test to make sure id is assigned to a player
+    #2 try removing the loser input and have a search that retrieves the loser's id
+    # and inputs that id into reportMatch() as loser.
+    #add a feature that has the user verify that the right id's are being updated.
     tournament.reportMatch(winner,loser)
-    for i in tournament.swissPairings():
-        print i
+    displayCurrentMatches()
 
 
 def displayCurrentMatches():
-
+    """
+       Displays the current matches taking place this round.
+    """
     for i in tournament.swissPairings():
         print "match: {}({}) vs {}({})".format(i[1], i[0], i[3], i[2])
+
+
+def startTournamentMenu():
+    """
+        menu to update each match. To view the standings and the match line ups
+    """
+    numberOfPlayers = tournament.countPlayers()
+    totalMatches = numberOfPlayers * (numberOfPlayers-1)
+    print numberOfPlayers, totalMatches
+    choice = ""
+    while choice != 'q':
+        print "How would you like to proceed:"
+        print "1) Display current matches"
+        print "2) Display player's current standings"
+        print "3) Update current matches"
+        print "q) quit"
+        choice = raw_input("> ")
+        if choice == 1:
+            displayCurrentMatches()
+        elif choice == 2:
+            displayStandings()
+        elif choice == 3:
+            updateMatches()
+        elif choice == 'q':
+            print "Exiting now"
+        else:
+            print "{} is an invalid choice, please try again".format(choice)
 
 
 if __name__ == "__main__":
     displayStandings()
     tournament.connect()
-   # updateMatches()
+    #updateMatches()
     displayCurrentMatches()
     menu()
