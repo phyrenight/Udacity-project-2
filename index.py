b8ad813aa94c1f7362244171b836eff8b6866e8e
Marc <phyrenight@gmail.com>
2016-05-18 06:37:44 -0400

    bug fixed pep errors
    ran code through pep8 validator and fixed issues.

--- index.py
+++ index.py
@@ -1,29 +1,31 @@
 import tournament
 
+
 def menu():
     """
         menu system for swiss pair tournament
     """
     choice = ""
-    while choice != 'q':
+    while choice != 'quit':
         print "What would you like to do: "
         print "1) add a player"
         print "2) start tournament"
         print "3) view  current players"
         print "4) reset database"
-        print "q) Enter q to quit"
+        print "Enter quit to exit"
         choice = raw_input("> ")
         if choice == '1':
             print "Enter a player's name:"
-            name = raw_input("> ") # make a check to ensure that a name is inputted
+            name = raw_input("> ")
             if name != "" or name != " ":
                 tournament.registerPlayer(name)
             else:
                 print "Invalid name"
-        elif choice == '2':   
+        elif choice == '2':
             numberOfPlayers = tournament.countPlayers()
             if numberOfPlayers < 6:
-                print "Insufficient number of players. Tournament can not be started."
+                print "Insufficient number of players. Tournament can \
+                       not be started."
             else:
                 print "Do you wish to start the tournament?"
                 print "You will not be able to add anymore players if you do."
@@ -34,20 +36,20 @@ def menu():
                 else:
                     print "start of tournament cancelled"
         elif choice == '3':
-        	players = tournament.playerStandings()
-        	for i in players:  # try turning these two lines into a list comprehension
-        	    print "player's name: {} id: {}".format(i[1], i[0])        # 2
+            players = tournament.playerStandings()
+            for i in players:
+                print "player's name: {} id: {}".format(i[1], i[0])
         elif choice == '4':
-            print " Are you sure you want to erase the database:(y/n)"
+            print " Are you sure you want to erase the database:(yes/no)"
             answer = raw_input("> ")
-            if answer == 'y':
-            	tournament.deleteMatches()
-            	tournament.deletePlayers()
-            elif answer == 'n':
-            	print "Deletion of database cancelled"
+            if answer == 'yes':
+                tournament.deleteMatches()
+                tournament.deletePlayers()
+            elif answer == 'no':
+                print "Deletion of database cancelled"
             else:
-            	print "Invalid input {}.".format(answer)
-        elif choice == 'q':
+                print "Invalid input {}.".format(answer)
+        elif choice == 'quit':
             print "Exiting now...."
         else:
             print "Invalid entry"
@@ -57,24 +59,29 @@ def displayStandings():
     """
         tournament standings are print to the screen
     """
-    results =  tournament.playerStandings()
+    results = tournament.playerStandings()
     print "Current standings are: "
     for i in results:
-        print "id:{} name: {} wins: {} loses: {} ".format(i[0], i[1], i[2], i[3])
+        print "id:{} name: {} wins: {} loses:\
+         {} ".format(i[0], i[1], i[2], i[3])
+
 
 def updateMatches():
     """
-        Used to to get wins/loses of the players. 
+        Used to to get wins/loses of the players.
     """
     displayCurrentMatches()
     print "Please enter the winners id "
-    winner = int(raw_input("> ")) # add test make sure id is in tournament
+    winner = int(raw_input("> "))  # add test make sure id is in tournament
     print "Print enter the losers id "
-    loser = int(raw_input("> ")) #1 add test to make sure id is assigned to a player
-    #2 try removing the loser input and have a search that retrieves the loser's id
+    loser = int(raw_input("> "))  # 1 add test to make sure id is
+    # assigned to a player
+    # 2 try removing the loser input and have a search that retrieves the
+    # loser's id
     # and inputs that id into reportMatch() as loser.
-    #add a feature that has the user verify that the right id's are being updated.
-    tournament.reportMatch(winner,loser)
+    # add a feature that has the user verify that the right id's
+    # are being updated.
+    tournament.reportMatch(winner, loser)
     displayCurrentMatches()
 
 
@@ -95,12 +102,12 @@ def startTournamentMenu():
     totalMatches = numberOfPlayers * (numberOfPlayers-1)
     print numberOfPlayers, totalMatches
     choice = ""
-    while choice != 'q':
+    while choice != 'quit':
         print "How would you like to proceed:"
         print "1) Display current matches"
         print "2) Display player's current standings"
         print "3) Update current matches"
-        print "q) quit"
+        print "Enter quit to exit"
         choice = raw_input("> ")
         if choice == 1:
             displayCurrentMatches()
@@ -108,7 +115,7 @@ def startTournamentMenu():
             displayStandings()
         elif choice == 3:
             updateMatches()
-        elif choice == 'q':
+        elif choice == 'quit':
             print "Exiting now"
         else:
             print "{} is an invalid choice, please try again".format(choice)
@@ -117,6 +124,6 @@ def startTournamentMenu():
 if __name__ == "__main__":
     displayStandings()
     tournament.connect()
-    #updateMatches()
+    # updateMatches()
     displayCurrentMatches()
     menu()
